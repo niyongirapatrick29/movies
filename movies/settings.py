dbc8d2df3130536a5bd3dbe6445e68f6d4e40179
Patrick NIYONGIRA <niyongirapatrick29@gmail.com>
2021-12-24 15:28:46 +0200

    heroku

--- movies/settings.py
+++ movies/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uulq6vs98dgbu&n5ts#%o2v%e_f@_x+$s9-05m-s8a!&!@rwg4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['movielibrary2022.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
